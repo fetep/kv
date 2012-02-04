5f5435a0bdbda449dddd62640014c00d7245693a
Pete Fritchman <petef@databits.net>
2012-02-04 11:41:44 -0500

    track mtime in KV::Node, support reloading when backing file changes

--- lib/kv/node.rb
+++ lib/kv/node.rb
@@ -1,11 +1,15 @@
 class KV; class Node
   attr_reader :attrs
+  attr_reader :mtime
+  attr_reader :name
+  attr_reader :path
 
   public
   def initialize(name, path)
     @name, @path = name, path
+    @mtime = 0
     @attrs = {}
-    load_attrs(path)
+    load_attrs
   end # def initialize
 
   public
@@ -13,21 +17,38 @@ class KV; class Node
     return @attrs[key]
   end
 
-  private
-  def load_attrs(path)
-    new_attrs = {}
-    File.open(path).each do |line|
-      if line == '' or line[0..0] == '#'
-        next
-      end
-
-      key, value = line.split(':')
-      new_attrs[key.strip] = value.strip
+  public
+  def reload
+    if changed?
+      load_attrs
     end
+  end
+
+  public
+  def changed?
+    stat = File.stat(@path) rescue nil
+    cur_mtime = stat ? stat.mtime : 0
+    return cur_mtime != @mtime
+  end
+
+  private
+  def load_attrs
+    new_attrs = {}
+    File.open(@path) do |file|
+      @mtime = file.stat.mtime
+      file.each do |line|
+        if line == '' or line[0..0] == '#'
+          next
+        end
+
+        key, value = line.split(':')
+        new_attrs[key.strip] = value.strip
+      end # file.each
+    end # File.open
     @attrs = new_attrs
   rescue Errno::ENOENT
-    # we have no attributes if there is no backing file to load
     @attrs = {}
+    @mtime = 0
   rescue
     raise KV::Error.new("node #{@name} failed to load from #{path}: #{$!}")
   end # def load_attrs
--- test/kv/test_node.rb
+++ test/kv/test_node.rb
@@ -19,5 +19,88 @@ describe KV::Node do
       n["key1"].should eq("value1")
       n["key2"].should eq("value2")
     end
+  end # describe initialize
+
+  describe '#changed?' do
+    it "should detect when the backing file mtime changes" do
+      node_path = File.join(@kvdb_path, "test")
+      File.open(node_path, "w+") do |f|
+        f.puts "key1: value1"
+      end
+      # move the mtime back in time a bit so our update changes it
+      File.utime(Time.now, Time.now - 10, node_path)
+      n = KV::Node.new("test", node_path)
+
+      File.open(node_path, "w+") do |f|
+        f.puts "key2: value2"
+      end
+      n.changed?.should eq(true)
+    end
+
+    it "should handle starting with no file and discovering one" do
+      node_path = File.join(@kvdb_path, "test")
+      n = KV::Node.new("test", node_path)
+
+      File.open(node_path, "w+") do |f|
+        f.puts "key1: value1"
+      end
+      n.changed?.should eq(true)
+    end
+
+    it "should handle starting with a file that gets deleted" do
+      node_path = File.join(@kvdb_path, "test")
+      File.open(node_path, "w+") do |f|
+        f.puts "key1: value1"
+      end
+      n = KV::Node.new("test", node_path)
+
+      File.unlink(node_path)
+      n.changed?.should eq(true)
+    end
+  end # describe changed?
+
+  describe '#reload' do
+    it "should reload all data when file changes" do
+      node_path = File.join(@kvdb_path, "test")
+      File.open(node_path, "w+") do |f|
+        f.puts "key1: value1"
+      end
+      # move the mtime back in time a bit so our update changes it
+      File.utime(Time.now, Time.now - 10, node_path)
+      n = KV::Node.new("test", node_path)
+      n["key1"].should eq("value1")
+
+      File.open(node_path, "w+") do |f|
+        f.puts "key2: value2"
+      end
+      n.reload
+      n["key1"].should eq(nil)
+      n["key2"].should eq("value2")
+    end
+
+    it "should clear data when a file is removed" do
+      node_path = File.join(@kvdb_path, "test")
+      File.open(node_path, "w+") do |f|
+        f.puts "key1: value1"
+      end
+      n = KV::Node.new("test", node_path)
+      n["key1"].should eq("value1")
+
+      File.unlink(node_path)
+      n.reload
+      n.attrs.should eq({})
+    end
+
+    it "should discover and load a new file" do
+      node_path = File.join(@kvdb_path, "test")
+      n = KV::Node.new("test", node_path)
+      n.attrs.should eq({})
+
+      File.open(node_path, "w+") do |f|
+        f.puts "key1: value1"
+      end
+      n.reload
+      n["key1"].should eq("value1")
+    end
   end
-end
+end # describe KV::Node
